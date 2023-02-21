43bb0a0ccd58d71376aa912b4edbe321bd7f72fa
Zsofie <zsofia@linumlabs.com>
2023-02-21 15:28:42 -0800

    added hardhat gas reporter

---
+++ hardhat.config.js
@@ -0,0 +1,76 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-contract-sizer");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("solidity-coverage");
+require("@openzeppelin/hardhat-upgrades");
+
+require("dotenv").config();
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+            details: {
+              yul: true,
+            },
+          },
+          viaIR: false,
+        },
+      },
+    ],
+  },
+  loggingEnabled: true,
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 31,
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API}`,
+        blockNumber: 15890332,
+      },
+      accounts: {
+        accountsBalance: "1000000000000000000000000", //1 million ETH to signers
+      },
+      gasPrice: 20036132356,
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API}`,
+      accounts: [`${process.env.MAINNET_DEPLOYER_PRIV_KEY}`],
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API}`,
+      accounts: [`${process.env.GOERLI_DEPLOYER_PRIV_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 120000,
+  },
+};
