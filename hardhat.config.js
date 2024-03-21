af985304cbe8d899bc4c1f250a7bd5f2c72906b8
Zsofie <zsofia@linumlabs.com>
2024-03-21 09:54:28 -0400

    last commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -64,6 +64,11 @@ module.exports = {
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API}`,
       accounts: [`${process.env.GOERLI_DEPLOYER_PRIV_KEY}`],
     },
+
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API}`,
+      accounts: [`${process.env.SEPOLIA_DEPLOYER_PRIV_KEY}`],
+    },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
